e6ad6d90f3dbe29a24da4a8600dc701fdd54647c
morango9 <105944114+morango9@users.noreply.github.com>
2024-04-26 11:46:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length; i++){
     botoes[i] .onclick = function(){
 
     for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
-    console.log(i);
+   
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05");
+
+contadores[0].textContent = "contagem regressiva";
 
